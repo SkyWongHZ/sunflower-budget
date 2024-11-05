60e59b85891425d84c4eea53281ec3ecec7391c6
sky <skywong0571@gmail.com>
2024-11-05 17:56:55 +0800

    feat:自动生成标签和账单模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { LoggerModule } from './common/logger/logger.module';
+import { RecordsModule } from './records/records.module';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [ UsersModule,LoggerModule],
+  imports: [ UsersModule,LoggerModule, RecordsModule, TagsModule],
   controllers: [AppController],
   providers: [AppService],
 })
