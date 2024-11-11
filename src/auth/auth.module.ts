6dd5201b5fb0c23787600a58d0b8e90a730787f0
sky <skywong0571@gmail.com>
2024-11-11 12:17:03 +0800

    feat:添加路由守卫

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,13 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from '../prisma/prisma.service';
+import { jwtConstants } from './constants';
 
 @Module({
   imports: [
     JwtModule.register({
       global: true,
-      secret: 'jwtConstants.secret',
+      secret: jwtConstants.secret,
       signOptions: { expiresIn: '7d' },
     }),
   ],
