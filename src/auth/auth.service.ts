c3b05b22c24bd89bede272282428f7ea958dd467
sky <skywong0571@gmail.com>
2024-11-11 11:33:46 +0800

    feat:加入jwt鉴权

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,23 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import {LoginDto} from   './dto/login.dto'
-import  *  as bcrypt from  'bcrypt'
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
 import { PrismaService } from '../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private  readonly   prisma: PrismaService)  {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
   async login(loginDto: LoginDto) {
     const hashedPassword = await bcrypt.hash(loginDto.password, 10);
-    const token = '123';
+
     const user = await this.prisma.user.findUnique({
-      where: {   username: loginDto.username },  
+      where: { username: loginDto.username },
 
       select: {
         id: true,
         username: true,
       },
     });
-    console.log('user',user);
+    console.log('user', user);
+    const payload = { sub: user, username: user.username };
+    const token = await this.jwtService.signAsync(payload);
     return {
       token,
       user,
